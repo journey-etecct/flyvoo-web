ca05e16ecf1faaf436ca74dc867aabf054d79738
oculosdanilo <oculosdanilo@gmail.com>
2023-09-08 18:22:44 -0300

    09/08/2023 18:22:43

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,22 +7,42 @@ import Contato from "./contato";
 import Sobre from "./sobre";
 import EntrarCadastro from "./entrarCadastro";
 import { AnimatePresence } from "framer-motion";
-import { useState } from "react";
+
+var logado: boolean = true;
 
 export default function Root() {
-  return (
-    <Router>
-      <Navbar />
-      <div className="testa">
-        <AnimatePresence>
-          <Routes>
-            <Route path="/" Component={Inicio} index></Route>
-            <Route path="/sobre" Component={Sobre}></Route>
-            <Route path="/contato" Component={Contato}></Route>
-            <Route path="/entrar" Component={EntrarCadastro}></Route>
-          </Routes>
-        </AnimatePresence>
-      </div>
-    </Router>
-  );
+  if (logado)
+    return (
+      <Router>
+        {/* USUÁRIO NÃO LOGADO */}
+        <Navbar />
+        <div className="testa">
+          <AnimatePresence>
+            <Routes>
+              <Route path="/" Component={Inicio} index></Route>
+              <Route path="/sobre" Component={Sobre}></Route>
+              <Route path="/contato" Component={Contato}></Route>
+              <Route path="/entrar" Component={EntrarCadastro}></Route>
+            </Routes>
+          </AnimatePresence>
+        </div>
+      </Router>
+    );
+  else
+    return (
+      <Router>
+        {/* USUÁRIO LOGADO */}
+        <Navbar />
+        <div className="testa">
+          <AnimatePresence>
+            <Routes>
+              <Route path="/" Component={Inicio} index></Route>
+              <Route path="/sobre" Component={Sobre}></Route>
+              <Route path="/contato" Component={Contato}></Route>
+              <Route path="/entrar" Component={EntrarCadastro}></Route>
+            </Routes>
+          </AnimatePresence>
+        </div>
+      </Router>
+    );
 }
