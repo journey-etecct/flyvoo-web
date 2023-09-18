4a97f77522dfdcc18d4863ee4ed1d74f65735b7d
oculosdanilo <oculosdanilo@gmail.com>
2023-09-18 09:27:11 -0300

    09/18/2023 09:27:11

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Background } from "../components/background";
+import Background from "../components/background";
 
 const inter = Inter({ subsets: ["latin"] });
 
