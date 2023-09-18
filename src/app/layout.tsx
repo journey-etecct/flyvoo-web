255ed8c66e43c486eba01c8b086b8dd65a65f8a2
oculosdanilo <oculosdanilo@gmail.com>
2023-09-18 00:04:29 -0300

    09/18/2023 00:04:29

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Flyvoo",
   description: "Site para decisão precisa da sua vida profissional.",
+  creator: "Journey",
 };
 
 export default function RootLayout({
