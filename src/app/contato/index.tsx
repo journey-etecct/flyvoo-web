49a611ba910c772cf542850ab8fa4efcaa41b7ed
oculosdanilo <oculosdanilo@gmail.com>
2023-09-18 20:44:32 -0300

    branch Sobre mergida e pequena correção no Contato

--- src/app/contato/index.tsx
+++ src/app/contato/index.tsx
@@ -95,6 +95,23 @@ export default function Contato() {
             </div>
           </div>
           <div className={styles.mapaContainer}>
+            <svg
+              className={styles.spinner}
+              width="65px"
+              height="65px"
+              viewBox="0 0 66 66"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle
+                className={styles.path}
+                fill="none"
+                strokeWidth="6"
+                strokeLinecap="round"
+                cx="33"
+                cy="33"
+                r="30"
+              ></circle>
+            </svg>
             <div id="google-map" className={styles.mapa}></div>
           </div>
         </div>
