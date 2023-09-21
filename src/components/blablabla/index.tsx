727a5c0930173427706b2c3ee9a86c928425be7a
oculosdanilo <oculosdanilo@gmail.com>
2023-09-21 18:05:47 -0300

    09/21/2023 18:05:47

--- src/components/blablabla/index.tsx
+++ src/components/blablabla/index.tsx
@@ -1,21 +1,29 @@
+import { Montserrat } from "next/font/google";
 import styles from "./index.module.css";
 import { motion } from "framer-motion";
+
+export const montserrat = Montserrat({ subsets: ["latin"] });
 
 export default function PopupPolitica() {
   return (
     <motion.div
       className={styles.popup}
       id="popup"
-      initial={{ right: "-100px" }}
+      /* initial={{ right: "-500px" }} */
       animate={{ right: "20px" }}
-      transition={{ ease: "circIn", delay: 0.5 }}
-    > 
-      <span>texto</span>
-      <br />
-      <span>texto</span>
-      <div className={styles.botoes}>
-        <div className={styles.botaoAceitar}>botao1</div>
-        <div className={styles.botaoLer}>botao2</div>
+      transition={{ ease: [0, 0.71, 0.2, 1.01], delay: 1, duration: 0.8 }}
+    >
+      <span className={`${styles.title} ${montserrat.className}`}>
+        Sua Privacidade
+      </span>
+      <span className={`${styles.content} ${montserrat.className}`}>
+        Ao se cadastrar, você concorda com nossos
+        <br />
+        Termos de Uso e Política de Privacidade
+      </span>
+      <div className={`${styles.botoes} ${montserrat.className}`}>
+        <div className={styles.botaoAceitar}>Aceito</div>
+        <div className={styles.botaoLer}>Ler Termos</div>
       </div>
     </motion.div>
   );
