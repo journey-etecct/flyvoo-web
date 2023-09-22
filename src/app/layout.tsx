88b416a58d0c37d6aa6fae3a8f2c36ffc3e54645
oculosdanilo <oculosdanilo@gmail.com>
2023-09-22 15:24:44 -0300

    começo do design do diálogo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <head>
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" sizes="any" />
       </head>
       <body className={inter.className}>
         <Background />
--- src/components/blablabla/index.tsx
+++ src/components/blablabla/index.tsx
@@ -1,30 +1,63 @@
 import { Montserrat } from "next/font/google";
 import styles from "./index.module.css";
 import { motion } from "framer-motion";
+import { useState } from "react";
 
 export const montserrat = Montserrat({ subsets: ["latin"] });
 
 export default function PopupPolitica() {
+  const [aparecerPopup, setAparecerPopup] = useState(true);
+
   return (
-    <motion.div
-      className={styles.popup}
-      id="popup"
-      /* initial={{ right: "-500px" }} */
-      animate={{ right: "20px" }}
-      transition={{ ease: [0, 0.71, 0.2, 1.01], delay: 1, duration: 0.8 }}
-    >
-      <span className={`${styles.title} ${montserrat.className}`}>
-        Sua Privacidade
-      </span>
-      <span className={`${styles.content} ${montserrat.className}`}>
-        Ao se cadastrar, você concorda com nossos
-        <br />
-        Termos de Uso e Política de Privacidade
-      </span>
-      <div className={`${styles.botoes} ${montserrat.className}`}>
-        <div className={styles.botaoAceitar}>Aceito</div>
-        <div className={styles.botaoLer}>Ler Termos</div>
-      </div>
-    </motion.div>
+    <>
+      <Popup />
+      {aparecerPopup ? <Politica /> : <></>}
+    </>
   );
+
+  function Politica() {
+    return (
+      <motion.div className={styles.fundo}>
+        <motion.div className={styles.dialogo}>
+          <p
+            onClick={() => {
+              setAparecerPopup(false);
+            }}
+          >
+            fechar
+          </p>
+        </motion.div>
+      </motion.div>
+    );
+  }
+
+  function Popup() {
+    return (
+      <motion.div
+        className={styles.popup}
+        id="popup"
+        /* initial={{ right: "-500px" }} */
+        animate={{ right: "20px" }}
+        transition={{ ease: [0, 0.71, 0.2, 1.01], delay: 1, duration: 0.8 }}
+      >
+        <span className={`${styles.title} ${montserrat.className}`}>
+          Sua Privacidade
+        </span>
+        <span className={`${styles.content} ${montserrat.className}`}>
+          Ao se cadastrar, você concorda com nossos
+          <br />
+          Termos de Uso e Política de Privacidade
+        </span>
+        <div className={`${styles.botoes} ${montserrat.className}`}>
+          <div
+            className={styles.botaoAceitar}
+            onClick={() => setAparecerPopup(true)}
+          >
+            Aceito
+          </div>
+          <div className={styles.botaoLer}>Ler Termos</div>
+        </div>
+      </motion.div>
+    );
+  }
 }
