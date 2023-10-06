8f634b8e0b37850f56e784a5f2ed0da60fcbc528
Danilo Lima <87551338+oculosdanilo@users.noreply.github.com>
2023-10-05 21:50:26 -0300

    animação na home  e começo da aba mais

--- src/app/mais/index.tsx
+++ src/app/mais/index.tsx
@@ -1,10 +1,32 @@
 import { motion } from "framer-motion";
 import styles from "./index.module.css";
+import { reverse } from "@/components/navbar_home";
+import { Route, Routes } from "react-router-dom";
+import Senha from "./senha";
+import Conta from "./conta";
+import Privacidade from "./privacidade";
+import CentralAjuda from "./centralAjuda";
 
 export default function Mais() {
   return (
-    <motion.div style={{ display: "flex", width: "100%", height: "100%" }}>
-      <div className={styles.main}></div>
+    <motion.div
+      style={{
+        display: "flex",
+        width: "100%",
+        height: "100%",
+        position: "absolute",
+      }}
+      initial={{ opacity: 0, top: reverse ? -50 : 50 }}
+      animate={{ opacity: 1, top: 0 }}
+    >
+      <div className={styles.main}>
+        <Routes>
+          <Route path="/" Component={Conta} />
+          <Route path="senha" Component={Senha} />
+          <Route path="privacidade" Component={Privacidade} />
+          <Route path="central" Component={CentralAjuda} />
+        </Routes>
+      </div>
       <div className={styles.sidenav}></div>
     </motion.div>
   );
