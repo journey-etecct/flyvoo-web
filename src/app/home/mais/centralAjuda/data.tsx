0ef70f0deae37141099a09627d7f4c5b0ff9d7f7
Danilo Lima <87551338+oculosdanilo@users.noreply.github.com>
2023-10-15 22:54:20 -0300

    tela mais terminadinha <3

--- src/app/home/mais/centralAjuda/data.tsx
+++ src/app/home/mais/centralAjuda/data.tsx
@@ -1,8 +1,11 @@
-const DATA: (string | string[])[][] = [
-  [
-    "Esqueci minha senha, como posso redefini-la?",
-    "Você pode redefinir sua senha clicando no botão “Esqueci minha senha” na tela de login. Em seguida, você receberá um e-mail com um link para criar uma nova senha. Lembre-se de escolher uma senha forte e fácil de lembrar.",
-    [
+type Pergunta = { pergunta: string; resposta: string; keywords: string[] };
+
+const DATA: Pergunta[] = [
+  {
+    pergunta: "Esqueci minha senha, como posso redefini-la?",
+    resposta:
+      "Você pode redefinir sua senha clicando no botão “Esqueci minha senha” na tela de login. Em seguida, você receberá um e-mail com um link para criar uma nova senha. Lembre-se de escolher uma senha forte e fácil de lembrar.",
+    keywords: [
       "esqueci",
       "senha",
       "redefinir",
@@ -18,11 +21,12 @@ const DATA: (string | string[])[][] = [
       "e-mail",
       "link",
     ],
-  ],
-  [
-    "Posso usar o aplicativo em vários dispositivos?",
-    "Sim, você pode usar o aplicativo em vários dispositivos, desde que você faça login com a mesma conta. Assim, você poderá acessar seus dados e resultados em qualquer dispositivo com acesso à internet.",
-    [
+  },
+  {
+    pergunta: "Posso usar o aplicativo em vários dispositivos?",
+    resposta:
+      "Sim, você pode usar o aplicativo em vários dispositivos, desde que você faça login com a mesma conta. Assim, você poderá acessar seus dados e resultados em qualquer dispositivo com acesso à internet.",
+    keywords: [
       "dispositivos",
       "vários",
       "conta",
@@ -38,11 +42,13 @@ const DATA: (string | string[])[][] = [
       "outro",
       "dispositivo",
     ],
-  ],
-  [
-    "O aplicativo possui um recurso de backup ou sincronização de dados?",
-    "Sim, todos os dados dos usuários do aplicativo são salvos na nuvem 24 horas e sempre disponíveis para o usuário a qualquer momento.",
-    [
+  },
+  {
+    pergunta:
+      "O aplicativo possui um recurso de backup ou sincronização de dados?",
+    resposta:
+      "Sim, todos os dados dos usuários do aplicativo são salvos na nuvem 24 horas e sempre disponíveis para o usuário a qualquer momento.",
+    keywords: [
       "backup",
       "sincronização",
       "sincronizar",
@@ -54,13 +60,12 @@ const DATA: (string | string[])[][] = [
       "aplicativo",
       "dado",
     ],
-  ],
-  [
-    "Posso personalizar as notificações que eu recebo?",
-
-    "Sim, na aba Mais vá em Minha Conta -> Configurações Gerais. A opção Minha Conta só está disponível para usuários cadastrados.",
-
-    [
+  },
+  {
+    pergunta: "Posso personalizar as notificações que eu recebo?",
+    resposta:
+      "Sim, na aba Mais vá em Minha Conta -> Configurações Gerais. A opção Minha Conta só está disponível para usuários cadastrados.",
+    keywords: [
       "notificações",
       "receber",
       "notificação",
@@ -72,13 +77,12 @@ const DATA: (string | string[])[][] = [
       "dispositivo",
       "cadastrado",
     ],
-  ],
-  [
-    "Como faço para encerrar minha conta no aplicativo?",
-
-    "Se você deseja encerrar sua conta no aplicativo, você pode fazê-lo na aba Mais do aplicativo, logo clique em Minha Conta -> Excluir minha conta [em vermelho]. Você precisará confirmar sua senha e aceitar os termos de encerramento da conta. Lembre-se que ao encerrar sua conta, você perderá todos os seus dados e resultados salvos no aplicativo.",
-
-    [
+  },
+  {
+    pergunta: "Como faço para encerrar minha conta no aplicativo?",
+    resposta:
+      "Se você deseja encerrar sua conta no aplicativo, você pode fazê-lo na aba Mais do aplicativo, logo clique em Minha Conta -> Excluir minha conta [em vermelho]. Você precisará confirmar sua senha e aceitar os termos de encerramento da conta. Lembre-se que ao encerrar sua conta, você perderá todos os seus dados e resultados salvos no aplicativo.",
+    keywords: [
       "encerrar",
       "deletar",
       "minha",
@@ -98,18 +102,26 @@ const DATA: (string | string[])[][] = [
       "confirmar",
       "confirmação",
     ],
-  ],
-  [
-    "O aplicativo está disponível offline?",
-    "Não, você precisa de uma conexão com a internet para usar o aplicativo e acessar seus dados e resultados.",
-    ["offline", "off-line", "off", "aplicativo", "internet", "acesso"],
-  ],
-  [
-    "Há um recurso de feedback no aplicativo para relatar problemas ou fornecer sugestões de melhoria?",
-
-    "Sim, você pode acessar esse recurso na aba Mais do aplicativo, ir em Minha Conta -> Nos dê um feedback! e enviar sua opinião ou reclamação. Nós agradecemos seu feedback e melhoraremos o aplicativo com base nele. O feedback só está disponível uma vez para cada usuário. Para mais informações mande um email para journeycompany2023@gmail.com",
-
-    [
+  },
+  {
+    pergunta: "O aplicativo está disponível offline?",
+    resposta:
+      "Não, você precisa de uma conexão com a internet para usar o aplicativo e acessar seus dados e resultados.",
+    keywords: [
+      "offline",
+      "off-line",
+      "off",
+      "aplicativo",
+      "internet",
+      "acesso",
+    ],
+  },
+  {
+    pergunta:
+      "Há um recurso de feedback no aplicativo para relatar problemas ou fornecer sugestões de melhoria?",
+    resposta:
+      "Sim, você pode acessar esse recurso na aba Mais do aplicativo, ir em Minha Conta -> Nos dê um feedback! e enviar sua opinião ou reclamação. Nós agradecemos seu feedback e melhoraremos o aplicativo com base nele. O feedback só está disponível uma vez para cada usuário. Para mais informações mande um email para journeycompany2023@gmail.com",
+    keywords: [
       "feedback",
       "Journey",
       "melhoria",
@@ -125,11 +137,13 @@ const DATA: (string | string[])[][] = [
       "sugestão",
       "aplicativo",
     ],
-  ],
-  [
-    "Como o aplicativo lida com a possibilidade de múltiplas áreas de identificação ou interesse?",
-    "Oferecemos algumas opções para você explorar e descobrir suas preferências e aptidões. Você pode fazer testes e atividades que irão ajudá-lo a conhecer melhor suas habilidades e interesses.",
-    [
+  },
+  {
+    pergunta:
+      "Como o aplicativo lida com a possibilidade de múltiplas áreas de identificação ou interesse?",
+    resposta:
+      "Oferecemos algumas opções para você explorar e descobrir suas preferências e aptidões. Você pode fazer testes e atividades que irão ajudá-lo a conhecer melhor suas habilidades e interesses.",
+    keywords: [
       "área",
       "múltiplas",
       "múltipla",
@@ -149,11 +163,13 @@ const DATA: (string | string[])[][] = [
       "ajuda",
       "habilidade",
     ],
-  ],
-  [
-    "Posso compartilhar os resultados obtidos através do aplicativo em redes sociais ou com outras pessoas?",
-    "Sim, você pode compartilhar os resultados obtidos através do aplicativo em redes sociais ou com outras pessoas. Você pode fazer isso clicando no botão “Compartilhar” na tela dos resultados e escolhendo a rede social ou o contato que deseja enviar seu resultado.",
-    [
+  },
+  {
+    pergunta:
+      "Posso compartilhar os resultados obtidos através do aplicativo em redes sociais ou com outras pessoas?",
+    resposta:
+      "Sim, você pode compartilhar os resultados obtidos através do aplicativo em redes sociais ou com outras pessoas. Você pode fazer isso clicando no botão “Compartilhar” na tela dos resultados e escolhendo a rede social ou o contato que deseja enviar seu resultado.",
+    keywords: [
       "compartilhar",
       "compartilhamento",
       "resultado",
@@ -165,11 +181,13 @@ const DATA: (string | string[])[][] = [
       "Whatsapp",
       "pessoal",
     ],
-  ],
-  [
-    "Como o aplicativo garante a privacidade e segurança das informações fornecidas?",
-    "Nós garantimos sua privacidade e segurança das informações fornecidas utilizando criptografia e protocolos seguros para proteger seus dados pessoais. O aplicativo também segue as normas legais vigentes sobre proteção de dados e não compartilha suas informações com terceiros sem sua autorização.",
-    [
+  },
+  {
+    pergunta:
+      "Como o aplicativo garante a privacidade e segurança das informações fornecidas?",
+    resposta:
+      "Nós garantimos sua privacidade e segurança das informações fornecidas utilizando criptografia e protocolos seguros para proteger seus dados pessoais. O aplicativo também segue as normas legais vigentes sobre proteção de dados e não compartilha suas informações com terceiros sem sua autorização.",
+    keywords: [
       "privacidade",
       "segurança",
       "informações",
@@ -178,11 +196,13 @@ const DATA: (string | string[])[][] = [
       "protocolos",
       "autorização",
     ],
-  ],
-  [
-    "Como o aplicativo de ajuda para descobrir a área que se identifica funciona?",
-    "Para descobrir a área que se identifica, utilizamos algoritmos inteligentes que analisam suas respostas aos testes, jogos, quiz e atividades que você realiza no aplicativo. O aplicativo então sugere as áreas que mais se adequam ao seu perfil, personalidade e preferências.",
-    [
+  },
+  {
+    pergunta:
+      "Como o aplicativo de ajuda para descobrir a área que se identifica funciona?",
+    resposta:
+      "Para descobrir a área que se identifica, utilizamos algoritmos inteligentes que analisam suas respostas aos testes, jogos, quiz e atividades que você realiza no aplicativo. O aplicativo então sugere as áreas que mais se adequam ao seu perfil, personalidade e preferências.",
+    keywords: [
       "aplicativo",
       "área",
       "identifica",
@@ -202,11 +222,12 @@ const DATA: (string | string[])[][] = [
       "perfil",
       "personalidade",
     ],
-  ],
-  [
-    "Como posso atualizar minhas informações pessoais no aplicativo?",
-    "Você pode atualizar suas informações pessoais no aplicativo nas configurações do seu perfil [Mais -> Minha Conta -> Editar Perfil]. Você pode alterar seu nome, gênero ou foto do jeito que desejar.",
-    [
+  },
+  {
+    pergunta: "Como posso atualizar minhas informações pessoais no aplicativo?",
+    resposta:
+      "Você pode atualizar suas informações pessoais no aplicativo nas configurações do seu perfil [Mais -> Minha Conta -> Editar Perfil]. Você pode alterar seu nome, gênero ou foto do jeito que desejar.",
+    keywords: [
       "informações",
       "informação",
       "pessoal",
@@ -219,14 +240,13 @@ const DATA: (string | string[])[][] = [
       "pronome",
       "pronomes",
     ],
-  ],
-  [
-    "Como faço para entrar em contato com o suporte ao cliente do aplicativo?",
-    [
-      "Basta mandar um Feedback na área Minha Conta ou mandar um email para ",
-      "journeycompany2023@gmail.com",
-    ],
-    [
+  },
+  {
+    pergunta:
+      "Como faço para entrar em contato com o suporte ao cliente do aplicativo?",
+    resposta:
+      "Basta mandar um Feedback na área Minha Conta ou mandar um email para journeycompany2023@gmail.com",
+    keywords: [
       "contato",
       "email",
       "e-mail",
@@ -237,7 +257,7 @@ const DATA: (string | string[])[][] = [
       "ajuda",
       "aplicativo",
     ],
-  ],
+  },
 ];
 
 export default DATA;
--- src/app/home/mais/centralAjuda/index.tsx
+++ src/app/home/mais/centralAjuda/index.tsx
@@ -2,8 +2,55 @@ import { reverse } from "@/components/navbar_mais";
 import { motion } from "framer-motion";
 import styles from "./index.module.css";
 import { poppins700 } from "@/app/entrarCadastro";
+import DATA from "./data";
+import { Divider, IconButton } from "@mui/material";
+import { useState } from "react";
 
 export default function CentralAjuda() {
+  const [aberto, setAberto] = useState<null | number>(null);
+
+  const listaPerguntas: JSX.Element[] = DATA.map((p) => {
+    return (
+      <>
+        <div key={p.pergunta} className={styles.pergunta}>
+          <p className={styles.tituloPerg}>
+            {DATA.indexOf(p) + 1}. {p.pergunta}
+          </p>
+          <IconButton
+            aria-label="expandir"
+            onClick={() => {
+              if (aberto == DATA.indexOf(p)) {
+                setAberto(null);
+              } else {
+                setAberto(DATA.indexOf(p));
+              }
+            }}
+          >
+            <motion.span
+              initial={false}
+              animate={{
+                rotate: aberto == DATA.indexOf(p) ? 180 : 0,
+              }}
+              className="material-symbols-rounded"
+            >
+              expand_more
+            </motion.span>
+          </IconButton>
+        </div>
+        <motion.div
+          className={styles.resposta}
+          initial={false}
+          animate={{
+            height: aberto == DATA.indexOf(p) ? "auto" : 0,
+          }}
+        >
+          <p className={styles.respostaTexto}>{p.resposta}</p>
+        </motion.div>
+        {p != DATA[DATA.length - 1] && <Divider className={styles.divider} />}
+      </>
+    );
+  });
+
   return (
     <motion.div
       style={{
@@ -17,6 +64,8 @@ export default function CentralAjuda() {
       <h1 className={styles.titulo} style={poppins700.style}>
         Central de Ajuda
       </h1>
+      <h2>Perguntas Frequentes</h2>
+      <div className={styles.scroll}>{listaPerguntas}</div>
     </motion.div>
   );
 }
