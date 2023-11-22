64c6f9c01e068484b544c8322483ef2fea555390
Danilo Lima <87551338+oculosdanilo@users.noreply.github.com>
2023-11-22 10:18:45 -0300

    iniciei a tela ursos

--- src/app/home/cursos/index.tsx
+++ src/app/home/cursos/index.tsx
@@ -1,12 +1,26 @@
 import { reverse } from "@/components/navbar_home";
 import { motion } from "framer-motion";
-import Image from "next/image";
+import styles from "./index.module.css";
+import { Nunito } from "next/font/google";
+import { useEffect, useState } from "react";
+import CardCursos from "@/components/cards_cursos";
+
+const nunito900 = Nunito({ subsets: ["latin"], weight: "900" });
 
 export default function Cursos() {
+  const [lista, setLista] = useState<number[]>([]);
+
+  useEffect(() => {
+    for (var i = 0; i == 50; i++) {
+      setLista((oldLista) => [...oldLista, i]);
+    }
+  }, [setLista]);
+
   return (
     <motion.div
       style={{
         display: "flex",
+        flexDirection: "column",
         width: "100%",
         height: "100%",
         position: "absolute",
@@ -14,7 +28,14 @@ export default function Cursos() {
       initial={{ opacity: 0, top: reverse ? -50 : 50 }}
       animate={{ opacity: 1, top: 0 }}
     >
-      <Image src="" alt="" width={80} height={80} />
+      <h1 style={nunito900.style} className={styles.titulo}>
+        Cursos
+      </h1>
+      <div className={styles.lista}>
+        {lista.map((index) => {
+          return <CardCursos key={index} index={index} />;
+        })}
+      </div>
     </motion.div>
   );
 }
---
+++ src/components/cards_cursos/index.tsx
@@ -0,0 +1,11 @@
+export default function CardCursos({ index }: { index: number }) {
+  return (
+    <div
+      style={{
+        fontSize: 40,
+      }}
+    >
+      {index}
+    </div>
+  );
+}
