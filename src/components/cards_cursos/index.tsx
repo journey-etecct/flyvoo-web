e7699a420d9d78aedc54a0141d4038229e2c090a
oculosdanilo <oculosdanilo@gmail.com>
2023-11-23 14:05:47 -0300

    teste

--- src/components/cards_cursos/index.tsx
+++ src/components/cards_cursos/index.tsx
@@ -2,7 +2,7 @@ export default function CardCursos({ index }: { index: number }) {
   return (
     <div
       style={{
-        fontSize: 40,
+        fontSize: "40px",
       }}
     >
       {index}
